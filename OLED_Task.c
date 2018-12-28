d05377793d259137f2a97f93950f90a6bb9b1d31
Thomas Neugschwandtner <ic17b082@technikum-wien.at>
2018-12-29 00:21:03 +0100

    suggestions

---
+++ OLED_Task.c
@@ -0,0 +1,37 @@
+/*
+ * OLED_C Task Version 1.0
+ * Thomas Neugschwandtner
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <inc/hw_memmap.h>
+
+/* XDCtools Header files */
+#include <xdc/std.h>
+#include <xdc/cfg/global.h>
+#include <xdc/runtime/System.h>
+#include <xdc/runtime/Error.h>
+#include <xdc/runtime/Memory.h>
+
+/* BIOS Header files */
+#include <ti/sysbios/BIOS.h>
+#include <ti/sysbios/knl/Event.h>
+#include <ti/sysbios/knl/Task.h>
+
+/* Driverlib headers */
+#include <driverlib/gpio.h>
+
+/* Board Header files */
+#include <Board.h>
+#include <EK_TM4C1294XL.h>
+
+#include <driverlib/ssi.h>
+#include <driverlib/sysctl.h>
+
+/* Application headers */
+#include "font.h"
+#include "UART_Task.h"
+
+
+
---
+++ local_inc/HW_init.h
@@ -0,0 +1,10 @@
+/*
+ * Hardware init???
+ */
+
+#ifndef HW_INIT_H_
+#define HW_INIT_H_
+
+void init_hardware(void);
+
+#endif /* HW_INIT_H_ */
